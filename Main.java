8877cb8a3d0f0094e6abade3489cb0dd16a016df
daniellynwon <62732930+daniellynwon@users.noreply.github.com>
2020-09-05 21:35:57 +0900

    백준 2438번 별 찍기 -1

--- Main.java
+++ Main.java
@@ -1,30 +1,20 @@
 package yuna;
 
-// 백준 11022번
+// 백준 2438번 별 찍기 -1
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램 작성.
-		// 첫재 줄에 테스트 케이스 개수 T가 주어짐.
-		// 0<A, B<10
-		// 테스트 케이스마다 'Case #x: A + B = C ' 형식으로 출력
-		int arr1[][];
-		int arr2[];
+		// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+		// 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+		// 첫째 줄부터 N번째 줄까지 차례대로 별 출력.
 		
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();		// 테스트 케이스 개수 받기.
-		arr1 = new int[t][t];		// a와 b 담을 배열
-		arr2 = new int[t];		// 테스트 케이스만큼 숫자 넣을 배열.
-		for(int i=0; i<t; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			arr1[i][0] = a;
-			arr1[i][1] = b;
-			arr2[i] = (a+b);		// 배열에 A,B 합한 값 넣기.
-		}
-		for(int i=0; i<t;i++) {
-			System.out.println("Case #"+(i+1)+": "+arr1[i][0]+" + "+arr1[i][1]+" = "+arr2[i]);			
+		int n = sc.nextInt();		// N 입력값.
+		for(int i=1;i<=n;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print("*");
+			} System.out.println("");
 		}
 	}
 }
