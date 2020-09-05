7c6849e3d1e6f4b9ab987aef161b9a113bd56ad4
daniellynwon <62732930+daniellynwon@users.noreply.github.com>
2020-09-05 11:21:45 +0900

    백준15552번

--- Main.java
+++ Main.java
@@ -1,22 +1,29 @@
-package test;
+package yuna;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
+	public static void main(String[] args) throws IOException {
 		
-		if(a<=100 && a>=90) {
-			System.out.println("A");
-		}else if(a<90 && a>=80) {
-			System.out.println("B");
-		}else if(a<80 && a>=70) {
-			System.out.println("C");
-		}else if(a<70 && a>=60) {
-			System.out.println("D");
-		}else {
-			System.out.println("F");
+		// 빠른 A+B bufferedReader, Writer 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine().trim());
+		
+		for(int i=0; i<num; i++) {
+			String str = br.readLine();
+			String[] txt = str.split(" ");		// 공백을 기준으로 string 짜름.
+			// 자른 string을 a,b로 나눠서 대입
+			int a = Integer.parseInt(txt[0]);
+			int b = Integer.parseInt(txt[1]);
+			bw.write((a+b)+"\n");
 		}
+		bw.flush();
+		bw.close();
+		
 	}
 }
